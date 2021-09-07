df2db397973d8e9c463478c56e5d8478ec889402
Maxim Mitin <maximus405@gmail.com>
2021-09-07 20:40:57 +0300

    Initial Commit

---
+++ HW_3.1/HW_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3_1App.swift
+//  HW_3.1
+//
+//  Created by Maxim Mitin on 7.09.21.
+//
+
+import SwiftUI
+
+@main
+struct HW_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
